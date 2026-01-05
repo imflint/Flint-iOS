fbbed103b7e457a38a22348fe60753362fdf1ba8
jinsoeun <rosejinse@naver.com>
2026-01-06 04:02:49 +0900

    [chore] placeholder 관련 extension 파일 분리

--- FLINT/FLINT/Presentation/Common/Extensions/UITextField+Padding.swift
+++ FLINT/FLINT/Presentation/Common/Extensions/UITextField+Padding.swift
@@ -9,28 +9,20 @@ import UIKit
 
 extension UITextField {
     
-    func setPlaceholder(color: UIColor) {
-        guard let string = self.placeholder else {
-            return
-        }
-        attributedPlaceholder = NSAttributedString(string: string, attributes: [.foregroundColor: color])
-    }
-    
-    func addPadding(_ width: CGFloat = 10) {
+    func addPadding(_ width: CGFloat = 12) {
         addLeftPadding(width)
         addRightPadding(width)
     }
     
-    func addLeftPadding(_ width: CGFloat = 10) {
-        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: width, height: self.frame.height))
+    func addLeftPadding(_ width: CGFloat = 12) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: width, height: 0))
         self.leftView = paddingView
         self.leftViewMode = ViewMode.always
     }
     
-    func addRightPadding(_ width: CGFloat = 10) {
-        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: width, height: self.frame.height))
+    func addRightPadding(_ width: CGFloat = 12) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: width, height: 0))
         self.rightView = paddingView
         self.rightViewMode = ViewMode.always
     }
-    
 }
