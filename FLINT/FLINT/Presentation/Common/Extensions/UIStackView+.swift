2af5e766853d2a529efffc12f21ebc2ed96ed30c
jinsoeun <rosejinse@naver.com>
2026-01-06 01:01:37 +0900

    [feat] extension 추가
    UIView, UIStackView, UITextField

---
+++ FLINT/FLINT/Presentation/Common/Extensions/UIStackView+.swift
@@ -0,0 +1,16 @@
+//
+//  UIStackView+.swift
+//  FLINT
+//
+//  Created by 진소은 on 1/5/26.
+//
+
+import UIKit
+
+extension UIStackView {
+    
+    func addArrangedSubviews(_ views: UIView...) {
+        views.forEach { self.addArrangedSubview($0) }
+    }
+    
+}
---
+++ FLINT/FLINT/Presentation/Common/Extensions/UITextField+Padding.swift
@@ -0,0 +1,36 @@
+//
+//  UITextField+Padding.swift
+//  FLINT
+//
+//  Created by 진소은 on 1/5/26.
+//
+
+import UIKit
+
+extension UITextField {
+    
+    func setPlaceholder(color: UIColor) {
+        guard let string = self.placeholder else {
+            return
+        }
+        attributedPlaceholder = NSAttributedString(string: string, attributes: [.foregroundColor: color])
+    }
+    
+    func addPadding(_ width: CGFloat = 10) {
+        addLeftPadding(width)
+        addRightPadding(width)
+    }
+    
+    func addLeftPadding(_ width: CGFloat = 10) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: width, height: self.frame.height))
+        self.leftView = paddingView
+        self.leftViewMode = ViewMode.always
+    }
+    
+    func addRightPadding(_ width: CGFloat = 10) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: width, height: self.frame.height))
+        self.rightView = paddingView
+        self.rightViewMode = ViewMode.always
+    }
+    
+}
