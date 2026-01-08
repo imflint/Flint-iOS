580fb0ac084760a2ca21a0706bdf10795575c0ee
Hosung.Kim <hosungkim5108@gmail.com>
2026-01-09 00:00:41 +0900

    [feat] SearchTextField 구현

---
+++ FLINT/FLINT/Presentation/Common/Components/SearchTextField.swift
@@ -0,0 +1,103 @@
+//
+//  SearchTextField.swift
+//  FLINT
+//
+//  Created by 김호성 on 2026.01.08.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class SearchTextField: UITextField {
+    
+    // MARK: - Property
+    
+    var searchAction: ((String?) -> Void)?
+    
+    // MARK: - Component
+    
+    private let actionButton = UIButton().then {
+        $0.setImage(.icSearch, for: .normal)
+    }
+    
+    // MARK: - Basic
+     
+    init(placeholder: String, searchAction: ((String?) -> Void)? = nil) {
+        self.searchAction = searchAction
+        super.init(frame: .zero)
+        
+        setUI()
+        setLayout()
+        setAction()
+        
+        applyFontStyle(.body1_r_16, textColor: .flintWhite)
+        self.placeholder = placeholder
+        applyPlaceholderFontStyle(.body1_r_16, textColor: .flintGray300)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = bounds.height / 2
+    }
+    
+    override func rightViewRect(forBounds bounds: CGRect) -> CGRect {
+        var padding = super.rightViewRect(forBounds: bounds)
+        padding.origin.x -= 12
+        return padding
+    }
+    
+    // MARK: - Setup
+    
+    private func setUI() {
+        backgroundColor = .flintGray700
+        tintColor = .flintGray300
+        
+        spellCheckingType = .no
+        autocapitalizationType = .none
+        autocorrectionType = .no
+        smartDashesType = .no
+        smartQuotesType = .no
+        smartInsertDeleteType = .no
+        if #available(iOS 17.0, *) {
+            inlinePredictionType = .no
+        }
+    }
+    
+    private func setLayout() {
+        addLeftPadding(16)
+        
+        rightView = actionButton
+        rightViewMode = .always
+        
+        snp.makeConstraints {
+            $0.height.equalTo(44)
+        }
+    }
+    
+    private func setAction() {
+        actionButton.addAction(UIAction(handler: touchUpInsideActionButton(_:)), for: .touchUpInside)
+        
+        addAction(UIAction(handler: { [weak self] _ in
+            self?.actionButton.setImage(.icCancel, for: .normal)
+        }), for: .editingDidBegin)
+        addAction(UIAction(handler: { [weak self] _ in
+            self?.actionButton.setImage(.icSearch, for: .normal)
+        }), for: .editingDidEnd)
+    }
+    
+    // MARK: - Function
+    
+    private func touchUpInsideActionButton(_ action: UIAction) {
+        if isFirstResponder {
+            text = nil
+        } else {
+            searchAction?(text)
+        }
+    }
+}
