f6969ae8e54850c54d675ffe1304f91d43ab3b3e
soonny <134983918+soeun11@users.noreply.github.com>
2026-01-13 20:49:30 +0900

    [chore] RemainingCountBadgeView 파일 분리

--- FLINT/FLINT/Presentation/Common/Components/RecentSaved/RecentSavedExampleView.swift
+++ FLINT/FLINT/Presentation/Common/Components/RecentSaved/RecentSavedExampleView.swift
@@ -11,7 +11,7 @@ import SnapKit
 import Then
 
 final class ExampleSavedContentViewController: UIViewController {
-
+    
     private let listView = SavedContentHorizontalListView()
 
     override func viewDidLoad() {
