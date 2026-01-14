a12093dd24bb9d31412f5af8fe609c911c55c1c0
soonny <134983918+soeun11@users.noreply.github.com>
2026-01-14 17:00:23 +0900

    [feat] savedContentIemVIew 초안완성

---
+++ FLINT/FLINT/Presentation/Common/Components/SavedItem/SavedContentItemView.swift
@@ -0,0 +1,325 @@
+//
+//  SavedContentItemView.swift
+//  FLINT
+//
+//  Created by 소은 on 1/14/26.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class SavedContentItemView: BaseView {
+
+    // MARK: - Mode
+
+    enum Mode: Equatable {
+        case ottShortcutBookmark
+        case selectable(isSelected: Bool)
+        case plain
+    }
+
+    // MARK: - Event
+
+    var onTapBookmark: (() -> Void)?
+    var onTapShortcut: (() -> Void)?
+    var onTapCheckbox: ((Bool) -> Void)?
+
+    // MARK: - Metric
+    
+    private var isBookmarked: Bool = false
+
+    private enum Metric {
+        static let posterSize = CGSize(width: 100, height: 150)
+        
+    }
+
+    // MARK: - State
+
+    private var mode: Mode = .ottShortcutBookmark
+    private var infoTrailingConstraint: Constraint?
+
+    // MARK: - UI
+
+    private let posterImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFill
+        $0.clipsToBounds = true
+        $0.backgroundColor = .flintGray100
+    }
+
+    private let ottLogoStripeView = OTTLogoStripeView().then {
+        $0.isHidden = true
+    }
+
+    private let infoContainerView = UIView()
+
+    private let titleLabel = UILabel().then {
+        $0.numberOfLines = 2
+        $0.lineBreakMode = .byTruncatingTail
+    }
+
+    private let directorLabel = UILabel().then {
+        $0.numberOfLines = 1
+        $0.lineBreakMode = .byTruncatingTail
+    }
+
+    private let yearLabel = UILabel().then {
+        $0.numberOfLines = 1
+        $0.lineBreakMode = .byTruncatingTail
+    }
+
+    private let shortcutButton = UIButton(type: .system).then {
+        $0.contentHorizontalAlignment = .leading
+        $0.isHidden = true
+    }
+
+    private let bookmarkButton = UIButton(type: .system).then {
+        $0.tintColor = .clear
+        $0.setImage(UIImage.icBookmarkFillWhite.withRenderingMode(.alwaysOriginal), for: .normal)
+        $0.isHidden = true
+    }
+    
+    private let bookmarkCountLabel = UILabel().then {
+        $0.numberOfLines = 1
+        $0.isHidden = true
+    }
+
+    private let checkboxButton = UIButton(type: .system).then {
+        $0.tintColor = .clear
+        $0.setImage(UIImage.icCheckboxEmpty.withRenderingMode(.alwaysOriginal), for: .normal)
+        $0.isHidden = true
+    }
+
+    // MARK: - BaseView
+
+    override func setUI() {
+        addSubviews(posterImageView, infoContainerView)
+
+        infoContainerView.addSubviews(titleLabel, directorLabel, yearLabel, shortcutButton)
+
+        addSubviews(bookmarkButton,bookmarkCountLabel,checkboxButton)
+        self.backgroundColor = .red
+
+        posterImageView.addSubview(ottLogoStripeView)
+
+        bookmarkButton.addTarget(self, action: #selector(didTapBookmark), for: .touchUpInside)
+        shortcutButton.addTarget(self, action: #selector(didTapShortcut), for: .touchUpInside)
+        checkboxButton.addTarget(self, action: #selector(didTapCheckbox), for: .touchUpInside)
+    }
+
+    override func setLayout() {
+        posterImageView.snp.makeConstraints {
+            $0.top.leading.bottom.equalToSuperview()
+            $0.width.equalTo(100)
+            $0.height.equalTo(150)
+        }
+
+        ottLogoStripeView.snp.makeConstraints {
+            $0.top.leading.equalToSuperview().inset(8)
+        }
+
+        infoContainerView.snp.makeConstraints {
+            $0.top.equalTo(posterImageView.snp.top)
+            $0.leading.equalTo(posterImageView.snp.trailing).offset(12)
+            $0.bottom.equalTo(posterImageView.snp.bottom)
+
+            infoTrailingConstraint = $0.trailing.equalToSuperview().inset(12).constraint
+        }
+
+        titleLabel.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+        }
+
+        directorLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(12)
+            $0.leading.trailing.equalToSuperview()
+        }
+
+        yearLabel.snp.makeConstraints {
+            $0.top.equalTo(directorLabel.snp.bottom).offset(0)
+            $0.leading.trailing.equalToSuperview()
+        }
+
+        shortcutButton.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+            $0.bottom.equalToSuperview()
+        }
+
+        bookmarkButton.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.top)
+            $0.leading.equalTo(titleLabel.snp.trailing).offset(16)
+            $0.trailing.equalToSuperview().inset(12)
+            $0.size.equalTo(24)
+        }
+
+        bookmarkCountLabel.snp.makeConstraints {
+            $0.top.equalTo(bookmarkButton.snp.bottom).offset(6)
+            $0.centerX.equalTo(bookmarkButton.snp.centerX)
+        }
+
+        checkboxButton.snp.remakeConstraints {
+            $0.centerY.equalTo(posterImageView.snp.centerY)
+            $0.leading.equalTo(infoContainerView.snp.trailing).inset(12)
+            $0.size.equalTo(48)
+        }
+    }
+
+    // MARK: - Configure
+
+    struct ViewModel {
+        let posterImage: UIImage?
+        let title: String
+        let director: String
+        let year: String
+
+        let isBookmarked: Bool
+        let bookmarkCount: Int?
+        let leadingPlatforms: [CircleOTTPlatform]?
+        let remainingPlatformCount: Int?
+        
+        
+
+        init(
+            posterImage: UIImage?,
+            title: String,
+            director: String,
+            year: String,
+            isBookmarked: Bool = false,
+            bookmarkCount: Int? = nil,
+            leadingPlatforms: [CircleOTTPlatform]? = nil,
+            remainingPlatformCount: Int? = nil
+        ) {
+            self.posterImage = posterImage
+            self.title = title
+            self.director = director
+            self.year = year
+            self.isBookmarked = isBookmarked
+            self.bookmarkCount = bookmarkCount
+            self.leadingPlatforms = leadingPlatforms
+            self.remainingPlatformCount = remainingPlatformCount
+        }
+    }
+
+    func configure(model: ViewModel, mode: Mode) {
+        self.mode = mode
+        
+ 
+        
+        posterImageView.image = model.posterImage
+
+        let titleStyle: UIFont.PretendardStyle
+
+        switch mode {
+        case .ottShortcutBookmark:
+            titleStyle = .body1_m_16
+        case .selectable, .plain:
+            titleStyle = .head3_sb_18
+        }
+        
+        let subsytleSyle: UIFont.PretendardStyle
+        
+        switch mode {
+        case .ottShortcutBookmark:
+            subsytleSyle = .caption1_m_12
+        case .selectable, .plain:
+            subsytleSyle = .body1_r_16
+        }
+
+        titleLabel.attributedText = .pretendard(titleStyle, text: model.title, color: .flintWhite)
+        directorLabel.attributedText = .pretendard(subsytleSyle, text: model.director, color: .flintGray300)
+        yearLabel.attributedText = .pretendard(subsytleSyle, text: model.year, color: .flintGray300)
+        shortcutButton.configuration = nil
+        shortcutButton.setAttributedTitle(
+            .pretendard(.body2_m_14, text: "작품 보러가기 >", color: .flintWhite),
+            for: .normal
+        )
+
+        if let count = model.bookmarkCount {
+            bookmarkCountLabel.attributedText = .pretendard(.caption1_r_12, text: "\(count)", color: .flintWhite)
+        } else {
+            bookmarkCountLabel.attributedText = nil
+        }
+
+        if let platforms = model.leadingPlatforms {
+            let remaining = model.remainingPlatformCount ?? 0
+            ottLogoStripeView.configure(leading: platforms, remainingCount: remaining)
+        } else {
+            ottLogoStripeView.isHidden = true
+        }
+
+        applyMode(mode)
+    }
+
+    // MARK: - apply
+
+    private func applyMode(_ mode: Mode) {
+        ottLogoStripeView.isHidden = true
+        shortcutButton.isHidden = true
+
+        bookmarkButton.isHidden = true
+        bookmarkCountLabel.isHidden = true
+        checkboxButton.isHidden = true
+
+        switch mode {
+        case .ottShortcutBookmark:
+            ottLogoStripeView.isHidden = false
+            shortcutButton.isHidden = false
+
+            bookmarkButton.isHidden = false
+            bookmarkCountLabel.isHidden = false
+
+            infoTrailingConstraint?.deactivate()
+            infoContainerView.snp.makeConstraints {
+                infoTrailingConstraint = $0.trailing.equalTo(bookmarkButton.snp.leading).offset(-4).constraint
+            }
+
+        case let .selectable(isSelected):
+            checkboxButton.isHidden = false
+            updateCheckbox(isSelected: isSelected)
+
+            infoTrailingConstraint?.deactivate()
+            infoContainerView.snp.makeConstraints {
+                infoTrailingConstraint = $0.trailing.equalTo(checkboxButton.snp.leading).offset(-4).constraint
+                    $0.top.bottom.equalToSuperview().inset(24)
+            }
+
+        case .plain:
+            infoTrailingConstraint?.deactivate()
+            infoContainerView.snp.makeConstraints {
+                infoTrailingConstraint = $0.trailing.equalToSuperview().inset(12).constraint
+                $0.top.bottom.equalToSuperview().inset(24)
+            }
+        }
+    }
+    
+    private func updateBookmark(isBookmarked: Bool) {
+        let image = isBookmarked ? UIImage.icBookmarkFill : UIImage.icBookmarkFillWhite
+        bookmarkButton.setImage(image.withRenderingMode(.alwaysOriginal), for: .normal)
+    }
+
+    private func updateCheckbox(isSelected: Bool) {
+        let image = isSelected ? UIImage.icCheckboxFill : UIImage.icCheckboxEmpty
+        checkboxButton.setImage(image.withRenderingMode(.alwaysOriginal), for: .normal)
+    }
+
+    // MARK: - Actions
+
+    @objc private func didTapBookmark() {
+        isBookmarked.toggle()
+        updateBookmark(isBookmarked: isBookmarked)
+        onTapBookmark?()
+    }
+
+    @objc private func didTapShortcut() {
+        onTapShortcut?()
+    }
+
+    @objc private func didTapCheckbox() {
+        guard case let .selectable(isSelected) = mode else { return }
+        let newSelected = !isSelected
+        self.mode = .selectable(isSelected: newSelected)
+        updateCheckbox(isSelected: newSelected)
+        onTapCheckbox?(newSelected)
+    }
+}
---
+++ FLINT/FLINT/Presentation/Common/Components/SavedItem/SearchResultWorkItemExampleViewController.swift
@@ -0,0 +1,130 @@
+//
+//  SearchResultWorkItemExampleViewController.swift
+//  FLINT
+//
+//  Created by 소은 on 1/14/26.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class SearchResultWorkItemExampleViewController: BaseViewController {
+
+    // MARK: - UI
+
+    private let scrollView = UIScrollView().then {
+        $0.showsVerticalScrollIndicator = true
+    }
+
+    private let contentView = UIView()
+
+    private let stackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.spacing = 20
+        $0.alignment = .fill
+        $0.distribution = .fill
+    }
+
+    private let version1View = SavedContentItemView()
+    private let version2View = SavedContentItemView()
+    private let version3View = SavedContentItemView()
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureExamples()
+        bindActions()
+    }
+
+    // MARK: - BaseViewController
+
+    override func setUI() {
+        view.backgroundColor = .black
+
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        contentView.addSubview(stackView)
+
+        stackView.addArrangedSubview(version1View)
+        stackView.addArrangedSubview(version2View)
+        stackView.addArrangedSubview(version3View)
+    }
+
+    override func setLayout() {
+        scrollView.snp.makeConstraints {
+            $0.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+
+        contentView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.width.equalToSuperview()
+        }
+
+        stackView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview().inset(16)
+            $0.bottom.equalToSuperview().inset(24)
+        }
+
+        // 각 아이템 뷰의 안정적으로 보이도록 최소 높이만 보장
+        [version1View, version3View, version2View].forEach {
+            $0.snp.makeConstraints { make in
+                make.height.greaterThanOrEqualTo(152) // 여유길이인데 이거 조정 필요함 !!
+            }
+        }
+    }
+
+    // MARK: - Configure
+
+    private func configureExamples() {
+        let dummyModelV1 = SavedContentItemView.ViewModel(
+            posterImage: UIImage(named: "poster_1"),
+            title: "은하수를 여행하는 히치하이커를 위한 안내서",
+            director: "가스 제닝스",
+            year: "2005",
+            bookmarkCount: 123,
+            leadingPlatforms: [.netflix, .coupangPlay],
+            remainingPlatformCount: 0
+        )
+
+        let dummyModelV2 = SavedContentItemView.ViewModel(
+            posterImage: UIImage(named: "poster_1"),
+            title: "은하수를 여행하는 히치하이커를 위한 안내서",
+            director: "가스 제닝스",
+            year: "2005"
+        )
+
+        let dummyModelV3 = SavedContentItemView.ViewModel(
+            posterImage: UIImage(named: "poster_1"),
+            title: "은하수를 여행하는 히치하이커를 위한 안내서",
+            director: "가스 제닝스",
+            year: "2005"
+        )
+
+        version1View.configure(model: dummyModelV1, mode: .ottShortcutBookmark)
+        version2View.configure(model: dummyModelV2, mode: .selectable(isSelected: false))
+        version3View.configure(model: dummyModelV3, mode: .plain)
+    }
+
+    private func bindActions() {
+        version1View.onTapBookmark = { [weak self] in
+            self?.toast("Bookmark")
+        }
+        version1View.onTapShortcut = { [weak self] in
+            self?.toast("Shortcut")
+        }
+
+        version2View.onTapCheckbox = { [weak self] isSelected in
+            self?.toast("Checkbox: \(isSelected)")
+        }
+
+    }
+
+    // MARK: - Debug Helper
+
+    private func toast(_ message: String) {
+        print("[SearchResultWorkItemExample] \(message)")
+    }
+}
