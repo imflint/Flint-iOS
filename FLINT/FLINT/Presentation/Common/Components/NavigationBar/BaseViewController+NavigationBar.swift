c928e8344aad2ec7b37af7f1ac77a9f69aedaec5
Hosung.Kim <hosungkim5108@gmail.com>
2026-01-16 21:06:13 +0900

    [hotfix] BaseViewController 사용성 개선

--- FLINT/FLINT/Presentation/Common/Components/NavigationBar/BaseViewController+NavigationBar.swift
+++ FLINT/FLINT/Presentation/Common/Components/NavigationBar/BaseViewController+NavigationBar.swift
@@ -10,39 +10,36 @@ import UIKit
 import SnapKit
 
 extension BaseViewController {
-    func installNavigationBarIfNeeded() {
-        navigationController?.setNavigationBarHidden(true, animated: false)
-
-        if navigationBarView.superview != nil { return }
-
-        view.addSubview(navigationBarView)
-        navigationBarView.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
-            $0.leading.trailing.equalToSuperview()
-            $0.height.equalTo(56)
-        }
-
-        navigationBarView.onTapLeft = { [weak self] in
-            guard let self else { return }
-            if let nav = self.navigationController, nav.viewControllers.count > 1 {
-                nav.popViewController(animated: true)
-            } else {
-                self.dismiss(animated: true)
-            }
-        }
-
-        navigationBarView.onTapRight = { [weak self] in
-            guard let self else { return }
-            self.dismiss(animated: true)
-        }
-    }
 
     func setNavigationBar(_ config: NavigationBarConfig,
                           onTapLeft: (() -> Void)? = nil,
                           onTapRight: (() -> Void)? = nil) {
-        installNavigationBarIfNeeded()
+        statusBarBackgroundView.isHidden = false
+        navigationBarView.isHidden = false
         navigationBarView.apply(config)
-        if let onTapLeft { navigationBarView.onTapLeft = onTapLeft }
-        if let onTapRight { navigationBarView.onTapRight = onTapRight }
+        updateStatusBarBackgroundColor(backgroundStyle: config.backgroundStyle)
+        navigationBarView.onTapLeft = onTapLeft ?? defaultLeftNavigationAction
+        navigationBarView.onTapRight = onTapRight ?? defaultRightNavigationAction
+    }
+    
+    private func updateStatusBarBackgroundColor(backgroundStyle: NavigationBarBackgroundStyle) {
+        switch backgroundStyle {
+        case .solid(let color):
+            statusBarBackgroundView.backgroundColor = color
+        case .clear:
+            statusBarBackgroundView.backgroundColor = .clear
+        }
+    }
+    
+    private func defaultLeftNavigationAction() {
+        if let nav = navigationController, nav.viewControllers.count > 1 {
+            nav.popViewController(animated: true)
+        } else {
+            dismiss(animated: true)
+        }
+    }
+    
+    private func defaultRightNavigationAction() {
+        dismiss(animated: true)
     }
 }
