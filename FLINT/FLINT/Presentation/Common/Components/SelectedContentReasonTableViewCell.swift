248a155852916c8b773f66093f4731f762e87d9a
soonny <134983918+soeun11@users.noreply.github.com>
2026-01-17 21:55:35 +0900

    [chore] 사진추가 파일 생성

---
+++ FLINT/FLINT/Presentation/Common/Components/SelectedContentReasonTableViewCell.swift
@@ -0,0 +1,17 @@
+
+//
+//  SelectedContentReasonTableViewCell.swift
+//  FLINT
+//
+//  Created by 소은 on 1/17/26.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class SelectedContentReasonTableViewCell: BaseTableViewCell {
+    
+    
+}
