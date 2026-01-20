23b3d9979d883b75040b308c4fb46dd61161d82b
Hosung.Kim <hosungkim5108@gmail.com>
2026-01-21 01:52:57 +0900

    [setting] Network Config 세팅

--- FLINT/Data/Sources/Networking/NetworkConfig.swift
+++ FLINT/Data/Sources/Networking/NetworkConfig.swift
@@ -10,11 +10,20 @@ import Foundation
 import Domain
 
 enum NetworkConfig {
+    static let resource: [String: String] = {
+        guard let file = Bundle.module.path(forResource: "Secret", ofType: "plist") else { fatalError("Secret.plist not found.") }
+        guard let resource = NSDictionary(contentsOfFile: file) as? [String: String] else { fatalError("Secret.plist not found.") }
+        return resource
+    }()
+    
     static let baseURL: URL = {
-        guard let baseURL = URL(string: "https://flint.r-e.kr") else {
-            Log.f("Invalid BaseURL")
-            fatalError("Invalid BaseURL")
-        }
+        guard let baseURLString = resource["base_url"] else { fatalError("base_url not found.") }
+        guard let baseURL = URL(string: baseURLString) else { fatalError("Invalid BaseURL") }
         return baseURL
+    }()
+    
+    static let testToken: String = {
+        guard let testToken = resource["test_token"] else { fatalError("test_token not found.") }
+        return testToken
     }()
 }
