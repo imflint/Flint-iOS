815f64ee94a8c2042f8b212e9b758e65bb4f0f2b
soonny <134983918+soeun11@users.noreply.github.com>
2026-01-21 04:42:45 +0900

    [network] 잠깐

---
+++ FLINT/Domain/Sources/Repository/ContentRepository.swift
@@ -0,0 +1,17 @@
+//
+//  ContentRepository.swift
+//  Domain
+//
+//  Created by 소은 on 1/21/26.
+//
+
+import Combine
+import Foundation
+
+import Entity
+
+public protocol ContentRepository {
+    // func fetchOTTPlatforms(_ contentId: Int64) -> AnyPublisher<FetchOTTPlatformsEntity, NetworkError>
+    func fetchBookmarkedContents() -> AnyPublisher<BookmarkContentsListEntity, NetworkError>
+}
+
