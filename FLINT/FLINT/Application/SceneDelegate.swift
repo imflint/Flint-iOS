a20e41847f1dd42de3626d380dffe86203eaf149
Hosung.Kim <hosungkim5108@gmail.com>
2026-03-03 13:13:27 +0900

    [refactor] 기본 파일들 폴더링

--- FLINT/FLINT/Application/SceneDelegate.swift
+++ FLINT/FLINT/Application/SceneDelegate.swift
@@ -22,7 +22,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(windowScene: windowScene)
         
-        let viewController = diContainer.makeSplashViewController()
+//        let viewController = diContainer.makeSplashViewController()
+        let viewController = diContainer.makeNicknameViewController()
 //        let viewController = diContainer.makeTabBarViewController()
         let navigationController = UINavigationController(rootViewController: viewController).configured({
             $0.navigationBar.isHidden = true
