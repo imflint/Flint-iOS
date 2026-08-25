8e750a6be64a8fba2dbfe95aa3b56e0f1499e456
soonny <134983918+soeun11@users.noreply.github.com>
2026-08-25 22:59:24 +0900

    [fix] 컬렉션 생성 시 이미지 추가 부분 이미지 보이는 비율/여백 수정

--- FLINT/Presentation/Sources/View/Component/SelectedContentReasonTableViewCell/SelectedContentReasonTableViewCell.swift
+++ FLINT/Presentation/Sources/View/Component/SelectedContentReasonTableViewCell/SelectedContentReasonTableViewCell.swift
@@ -343,32 +343,42 @@ public final class SelectedContentReasonTableViewCell: BaseTableViewCell {
             $0.clipsToBounds = true
             $0.backgroundColor = .flintGray800
         }
+
+        let imageRatio = image.size.width / image.size.height
+        let containerRatio = UIScreen.main.bounds.width / Metric.photoHeight
+
+        // 이미지 비율과 컨테이너 비율이 threshold배 이상 차이나면
+        // Crop 시 과도하게 잘리는 것으로 판단해 원본 비율 유지
+        let threshold: CGFloat = 1.6
+        let deviation = max(imageRatio, containerRatio) / min(imageRatio, containerRatio)
+        let isExtremeRatio = deviation > threshold
+
         let imageView = UIImageView().then {
-            $0.contentMode = .scaleAspectFit
+            $0.contentMode = isExtremeRatio ? .scaleAspectFit : .scaleAspectFill
             $0.clipsToBounds = true
             $0.image = image
         }
-        
+
         let deleteButton = UIButton().then {
             $0.setImage(.icBlackXmark, for: .normal)
             $0.tag = realIndex
             $0.addTarget(self, action: #selector(didTapDeletePhoto(_:)), for: .touchUpInside)
         }
-        
+
         wrapper.addSubviews(imageView, deleteButton)
-        
+
         imageView.snp.makeConstraints { $0.edges.equalToSuperview() }
-        
+
         deleteButton.snp.makeConstraints {
             $0.top.trailing.equalToSuperview().inset(12)
             $0.size.equalTo(48)
         }
-        
+
         wrapper.snp.makeConstraints {
             $0.width.equalTo(UIScreen.main.bounds.width)
             $0.height.equalTo(Metric.photoHeight)
         }
-        
+
         return wrapper
     }
     
