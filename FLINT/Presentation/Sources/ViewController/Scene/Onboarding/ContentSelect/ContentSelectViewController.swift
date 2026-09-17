6fc74ef27e5bdd85ffe113c424211ac7524bd270
Hosung Kim <hosungkim5108@gmail.com>
2026-09-17 14:48:41 +0900

    [fix] 필터 변경 시 스크롤 위치 최소화

--- FLINT/Presentation/Sources/ViewController/Scene/Onboarding/ContentSelect/ContentSelectViewController.swift
+++ FLINT/Presentation/Sources/ViewController/Scene/Onboarding/ContentSelect/ContentSelectViewController.swift
@@ -349,11 +349,13 @@ extension ContentSelectViewController {
     public func genreCollectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         onboardingViewModel.filterGenre.insert(Genre.allCases[indexPath.row])
         onboardingViewModel.searchContents()
+        rootView.contentCollectionView.scrollToItem(at: IndexPath(row: 0, section: 0), at: .top, animated: false)
     }
     
     public func genreCollectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
         onboardingViewModel.filterGenre.remove(Genre.allCases[indexPath.row])
         onboardingViewModel.searchContents()
+        rootView.contentCollectionView.scrollToItem(at: IndexPath(row: 0, section: 0), at: .top, animated: false)
     }
 }
 
